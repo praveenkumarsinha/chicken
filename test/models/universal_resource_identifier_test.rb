25dc7543b014525d26ef43dde658802355e617c2
Praveen Kumar Sinha <praveen.kumar.sinha@gmail.com>
2018-05-10 22:52:50 +0530

    1. Adding test case for short_url_id 2. Updating test case for long_url to accommodate short_url_id

--- test/models/universal_resource_identifier_test.rb
+++ test/models/universal_resource_identifier_test.rb
@@ -8,6 +8,7 @@ class UniversalResourceIdentifierTest < ActiveSupport::TestCase
 
   test 'should not save URI with invalid long_url' do
     uri = UniversalResourceIdentifier.new
+    uri.short_url_id = 'tes123'
 
     assert_not uri.valid?
     assert_equal ["can't be blank", "is not a valid http url"], uri.errors[:long_url]
@@ -17,6 +18,25 @@ class UniversalResourceIdentifierTest < ActiveSupport::TestCase
     assert_equal ["is not a valid http url"], uri.errors[:long_url]
 
     uri.long_url = "http://test-domain.com/some/path"
+    assert uri.save
+  end
+
+  test 'should not save URI with invalid short_url_id' do
+    uri = UniversalResourceIdentifier.new
+    uri.long_url = "http://test-domain.com/some/path"
+
+    assert_not uri.valid?
+    assert_equal ["can't be blank", "is too short (minimum is 3 characters)"], uri.errors[:short_url_id]
+
+    uri.short_url_id = 'te'
+    assert_not uri.valid?
+    assert_equal ["is too short (minimum is 3 characters)"], uri.errors[:short_url_id]
+
+    uri.short_url_id = 'test1234'
+    assert_not uri.valid?
+    assert_equal ["is too long (maximum is 6 characters)"], uri.errors[:short_url_id]
+
+    uri.short_url_id = 'tes123'
     assert uri.save
   end
 
