6b0ec088cd9c9a6c00a81e408fe66de6d8f3e7f8
Praveen Kumar Sinha <praveen.kumar.sinha@gmail.com>
2018-05-10 22:54:17 +0530

    1. Adding validations for short_url_id

--- app/models/universal_resource_identifier.rb
+++ app/models/universal_resource_identifier.rb
@@ -2,6 +2,7 @@ class UniversalResourceIdentifier < ApplicationRecord
 
   #==== Validations ===================================
   validates :long_url, presence: true, uniqueness: {case_sensitive: false}, http_url: true
+  validates :short_url_id, presence: true, uniqueness: true, length: {within: 3..6}
 
   #==== Callbacks =====================================
   before_validation :downcase_long_url
