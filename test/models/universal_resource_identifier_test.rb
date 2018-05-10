4d3bca42cf1ffa4452b4ec4220f6a66004d39b9b
Praveen Kumar Sinha <praveen.kumar.sinha@gmail.com>
2018-05-10 17:37:54 +0530

    1. Adding test to check for long_url presence

--- test/models/universal_resource_identifier_test.rb
+++ test/models/universal_resource_identifier_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class UniversalResourceIdentifierTest < ActiveSupport::TestCase
+
   # test "the truth" do
   #   assert true
   # end
+
+  test 'should not save URI with invalid long_url' do
+    uri = UniversalResourceIdentifier.new
+
+    assert_not uri.valid?
+    assert_equal ["can't be blank"], uri.errors[:long_url]
+  end
+
 end
