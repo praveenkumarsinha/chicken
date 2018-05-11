8ccaa2ac4034f9340b735b44707531b09a35f9df
Praveen Kumar Sinha <praveen.kumar.sinha@gmail.com>
2018-05-11 14:59:25 +0530

    1. Adding first functional prototype of shorten-d version of url

--- app/controllers/universal_resource_identifier_controller.rb
+++ app/controllers/universal_resource_identifier_controller.rb
@@ -1,7 +1,15 @@
 class UniversalResourceIdentifierController < ApplicationController
 
   def create
-    redirect_back(fallback_location: root_path)
+    @url = UniversalResourceIdentifier.find_or_create_by(long_url: params[:universal_resource_identifier][:long_url].try(:downcase))
   end
-  
+
+  def visit
+    if  url = UniversalResourceIdentifier.find_by(short_url_id: params[:short_url_id])
+      redirect_to(url.long_url)
+    else
+      head :not_found
+    end
+  end
+
 end
