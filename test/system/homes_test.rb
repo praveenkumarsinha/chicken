a59922982e179b03cc2d9b4fa258a60d47b34a29
Praveen Kumar Sinha <praveen.kumar.sinha@gmail.com>
2018-05-11 16:57:57 +0530

    1. Adding "Not good enough" test cases for "creating a shortend URL" system test case

--- test/system/homes_test.rb
+++ test/system/homes_test.rb
@@ -8,4 +8,11 @@ class HomesTest < ApplicationSystemTestCase
     assert_selector "input[type=submit]"
   end
 
+  test "creating a shortend URL" do
+    visit root_url
+    fill_in "universal_resource_identifier[long_url]", with: "https://getbootstrap.com/docs/4.0/components/forms"
+    click_button "Short it!"
+    assert_current_path root_path
+  end
+
 end
