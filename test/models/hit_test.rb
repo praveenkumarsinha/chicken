034cfe94f19a414ac746ec7f7e6e85eebfb0da97
Praveen Kumar Sinha <praveen.kumar.sinha@gmail.com>
2018-05-11 08:21:16 +0530

    1. Adding unit test case (for hit model) to test presence of universal_resource_identifier

--- test/models/hit_test.rb
+++ test/models/hit_test.rb
@@ -4,4 +4,11 @@ class HitTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test 'should not save without universal_resource_identifier' do
+    hit = Hit.new
+    assert_not hit.valid?
+    assert_equal ["must exist"], hit.errors[:universal_resource_identifier]
+  end
+
 end
