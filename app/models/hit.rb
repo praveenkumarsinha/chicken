36466cf063c32a39f550702251d649cb990c8be1
Praveen Kumar Sinha <praveen.kumar.sinha@gmail.com>
2018-05-11 08:49:02 +0530

    1. Adding unit test case (for hit model) to test validation of bot_hit, ip_address and request_dump

--- app/models/hit.rb
+++ app/models/hit.rb
@@ -1,5 +1,9 @@
 class Hit < ApplicationRecord
 
+  #==== Validations ===================================
+  validates :ip_address, :request_dump, presence: true
+  validates :bot_hit, inclusion: {in: [true, false], message: 'should be either true or false'}
+
   #==== Associations ==================================
   belongs_to :universal_resource_identifier, inverse_of: :hits
 
