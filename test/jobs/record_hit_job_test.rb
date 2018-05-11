d6e96e41680af3fcfeba27d8be78da3d19904ba5
Praveen Kumar Sinha <praveen.kumar.sinha@gmail.com>
2018-05-11 21:44:16 +0530

    1. Adding test case for record_hit job

--- test/jobs/record_hit_job_test.rb
+++ test/jobs/record_hit_job_test.rb
@@ -4,4 +4,26 @@ class RecordHitJobTest < ActiveJob::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test 'hit is being created' do
+    url = universal_resource_identifiers(:first_one)
+    assert_equal 0, url.hits.count
+
+    user_agent = UserAgent.parse('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.56 Safari/536.5')
+
+    RecordHitJob.perform_now(url, {ip_address: '172.217.31.14',
+                                   bot_hit: false,
+                                   http_referer: nil,
+                                   browser: user_agent.browser,
+                                   version: user_agent.version.to_s,
+                                   platform: user_agent.platform,
+                                   request_dump: 'some large request dump'})
+
+    assert_equal 1, url.hits.count
+    assert_equal '172.217.31.14', url.hits.take.ip_address
+    assert_equal 'Chrome', url.hits.take.browser
+    assert_equal '19.0.1084.56', url.hits.take.version
+    assert_equal 'Macintosh', url.hits.take.platform
+  end
+
 end
